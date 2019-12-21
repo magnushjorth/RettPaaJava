33f765701440bf2a80d88cd11dd19ee321ad42a1
Magnus Hjorth <57001299+magnushjorth@users.noreply.github.com>
2019-12-21 23:27:23 +0100

    21.12.2019

--- Skihopp.java
+++ Skihopp.java
@@ -1,40 +1,46 @@
 import easyIO.*;
 
-class Skihopp {
-  public static void main(String[] args) {
-  	In tastatur = new In();
-  	double[] stilkarakterer = new double[5];
-  	double tabellpunkt = 120;
-  	double meterverdi = 1.8;
-  	double lengde, poeng;
-  	double stilpoeng = 0;
-  	double lengdepoeng;
-  	double stilmin, stilmaks;
+class Poengmetode {
+    public static void main(String[] args) {
+	     In tastatur = new In();
+	     double poeng = poengberegning(tastatur);
+	     System.out.println("Hopperen fikk " + poeng + " poeng");
+    }
+    public static double poengberegning(In tastatur) {
+	     double[] stilkarakterer = new double[5];
+	     double tabellpunkt = 120;
+	     double meterverdi = 1.8;
+	     double lengde, poeng;
+	     double stilpoeng = 0;
+	     double lengdepoeng;
+       double stilmin, stilmaks;
 
-  	System.out.print("Oppgi lengde : ");
-  	lengde = tastatur.inDouble();
+	     // leser inn hopplengde
+	     System.out.print("Oppgi lengde : ");
+	     lengde = tastatur.inDouble();
+	     // beregner lengdepoeng
+	     lengdepoeng = 60 + (lengde - tabellpunkt)*meterverdi;
 
-  	lengdepoeng = 60 + (lengde - tabellpunkt)*meterverdi;
+	     // leser inn stilkarakterer og summerer dem samtidig
+	     for (int i = 0; i < stilkarakterer.length; i ++) {
+	        System.out.print("Dommer nr " + (i+1) + " : ");
+	        stilkarakterer[i] = tastatur.inDouble();
+	        stilpoeng = stilpoeng + stilkarakterer[i];
+	     }
+	     // finner minste stilpoeng
+	     stilmin = stilkarakterer[0];
+	     for (int i = 1; i < stilkarakterer.length; i++) {
+	        if (stilmin > stilkarakterer[i]) stilmin = stilkarakterer[i];
+	     }
+ 	     // finner st�rste stilpoeng
+	     stilmaks = stilkarakterer[0];
+	     for (int i = 1; i < stilkarakterer.length; i++) {
+	        if (stilmaks < stilkarakterer[i]) stilmaks = stilkarakterer[i];
+	     }
+	     // trekker fra st�rste og minste i stilpoengene
+	     stilpoeng = stilpoeng - stilmin - stilmaks;
 
-  	for (int i = 0; i < stilkarakterer.length; i ++) {
-  	    System.out.print("Dommer nr " + (i+1) + " : ");
-  	    stilkarakterer[i] = tastatur.inDouble();
-  	    stilpoeng = stilpoeng + stilkarakterer[i];
-  	}
-
-  	stilmin = stilkarakterer[0];
-  	for (int i = 1; i < stilkarakterer.length; i++) {
-  	    if (stilmin > stilkarakterer[i]) stilmin = stilkarakterer[i];
-  	}
-
-  	stilmaks = stilkarakterer[0];
-  	for (int i = 1; i < stilkarakterer.length; i++) {
-  	    if (stilmaks < stilkarakterer[i]) stilmaks = stilkarakterer[i];
-  	}
-
-  	stilpoeng = stilpoeng - stilmin - stilmaks;
-
-  	poeng = stilpoeng + lengdepoeng;
-  	System.out.println("Hopperen fikk " + poeng + " poeng");
-  }
+	     // beregner totalpoeng
+	     return stilpoeng + lengdepoeng;
+   }
 }
