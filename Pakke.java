24859ffe50897dfda0026cbbfa0767516e08b236
Magnus Hjorth <57001299+magnushjorth@users.noreply.github.com>
2019-12-29 21:17:49 +0100

    29.12.2019

---
+++ Pakke.java
@@ -0,0 +1,16 @@
+class Pakke {
+  public static void main(String[] args) {
+    Integer b = new Integer(4);
+    Double d = new Double(3.6);
+    int j = 3;
+
+    int i = b + 1;
+    Integer a = j;
+    int k = a + b;
+    double x = d / a;
+    System.out.println("Integer a:" + a + ", b:" + b +
+           ",  Double d:" + d +
+           "\nint j:" + j + " i:" + i + ", k:" + k +
+           ",  double x:" + x);
+  }
+}
